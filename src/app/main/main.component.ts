dfbc2f6ee5097d94dc336b9c48421c68449f22c1
taylord109 <cisco.cat@hotmail.com>
2019-04-07 22:56:30 -0400

    Fixed navigation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  OnDestroy
+} from '@angular/core';
 import { MainService } from '../services/main.service';
 import { MainPage } from '../types/mainTypes';
 
@@ -7,10 +13,15 @@ import { MainPage } from '../types/mainTypes';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass']
 })
-export class MainComponent implements OnInit {
-
+export class MainComponent implements OnInit, OnDestroy {
   public currentPage: MainPage;
   public mainService: MainService;
+  @ViewChild('market') market: ElementRef;
+
+  public screenWidth = 0;
+  public screenHeight = 0;
+
+  public screenSizeSub;
 
   constructor(mainService: MainService) {
     this.mainService = mainService;
@@ -20,9 +31,22 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.market = this.market.nativeElement;
+
+    this.market['mute'] = true;
+    this.market['autoplay'] = true;
+    this.market['loop'] = true;
+
     this.currentPage = this.mainService.currentPage;
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+    this.screenSizeSub = window.addEventListener('resize', event => {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    });
   }
 
+  ngOnDestroy() {
+    this.screenSizeSub(); // Unsubscribe from the screen size listener
+  }
 }
-
-
